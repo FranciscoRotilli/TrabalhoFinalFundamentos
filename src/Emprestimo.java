26a4ee6aff10800c94ca0c878fd3c653350cf509
Francisco Coimbra Rotilli <francisco.r@edu.pucrs.br>
2024-11-18 18:16:11 -0300

    Inicial

---
+++ src/Emprestimo.java
@@ -0,0 +1,4 @@
+public class Emprestimo {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/Equipamento.java
@@ -0,0 +1,8 @@
+public class Equipamento {
+    private String codigo;
+    private String nome;
+    private int tipo;
+    private int quantidade;
+    private double valor;
+    private boolean seguro;
+}
