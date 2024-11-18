0b9834f65b8533dbb6418a734eddf8deac79e380
Francisco Coimbra Rotilli <francisco.r@edu.pucrs.br>
2024-11-18 18:36:10 -0300

    Construtor

--- src/Equipamento.java
+++ src/Equipamento.java
@@ -1,5 +1,5 @@
 public class Equipamento {
-    private String codigo;
+    private int codigo;
     private String nome;
     private int tipo;
     private int quantidade;
@@ -7,6 +7,14 @@ public class Equipamento {
     private boolean seguro;
 
     public Equipamento() {
-
+        codigo = 0;
+        nome = "";
+        tipo = 0;
+        quantidade = 0;
+        valor = 0;
+        seguro = false;
+    }
+    public Equipamento(int codigo, String nome, int tipo, int quantidade, double valor, boolean seguro) {
+        if (codigo > 0) this.codigo = 0;
     }
 }
